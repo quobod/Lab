74e017e5b3eafab4e600333026f66c2a385041fc
quobod <quobod@gmail.com>
2022-03-02 06:02:33 -0500

    Added input add and remove

--- public/assets/js/elements.js
+++ public/assets/js/elements.js
@@ -34,3 +34,6 @@ export const contactEditButton = document.querySelector("#contact-edit-button");
 export const contactDeleteButton = document.querySelector(
   "#contact-delete-button"
 );
+export const editContactForm = document.querySelector("#edit-contact-form");
+export const addEmailButton = document.querySelector("#add-email");
+export const addPhoneButton = document.querySelector("#add-phone");
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,12 @@
 import * as elements from "./elements.js";
-import { addHandler, log, getAttribute } from "./utils.js";
+import {
+  addHandler,
+  log,
+  getAttribute,
+  newElement,
+  appendChild,
+  addAttribute,
+} from "./utils.js";
 import * as wss from "./wss.js";
 
 // init socket connection
@@ -174,22 +181,90 @@ if (document.title.toLowerCase().trim() == "dashboard") {
     const attribute = getAttribute(target, "data-rmtId");
     log(`\n\t${target.id}'s data: ${attribute}`);
   });
-
-  /*  addHandler(elements.contactEditButton, "click", (e) => {
-    const target = e.target;
-    log(`\n\t${target.id || target} clicked`);
-    const attribute = getAttribute(target, "data-rmtId");
-    log(`\n\t${target.id}'s data: ${attribute}`);
-
-    const xhttp = new XMLHttpRequest();
-
-    xhttp.onload = () => {};
-
-    xhttp.open(
-      "GET",
-      `http://localhost:3000/user/contacts/${attribute.trim()}`
-    );
-
-    xhttp.send();
-  }); */
 }
+
+addHandler(elements.addEmailButton, "click", () => {
+  log(`\n\tAdded email field\n`);
+  const editContactForm = elements.editContactForm;
+
+  const divInputGroup = newElement("div");
+  const divGroup = newElement("div");
+  const spanInputGroupLabel = newElement("span");
+  const emailInput = newElement("input");
+  const trashIcon = newElement("i");
+  const emailIcon = newElement("i");
+
+  // Add attributes
+  addAttribute(divGroup, "class", "grid-x");
+  addAttribute(divInputGroup, "class", "input-group");
+  addAttribute(spanInputGroupLabel, "class", "input-group-label");
+  addAttribute(emailInput, "type", "email");
+  addAttribute(emailInput, "class", "input-group-field");
+  addAttribute(
+    emailInput,
+    "name",
+    `email${editContactForm.childNodes.length + 1}`
+  );
+  addAttribute(trashIcon, "class", "fa-solid fa-trash");
+  addAttribute(emailIcon, "class", "fa-solid fa-envelope");
+  addAttribute(trashIcon, "style", "margin-left:5px;margin-top:10px; ");
+
+  // Config elements
+  // spanInputGroupLabel.innerHTML = `<i class="fa-solid fa-envelope"></i>`;
+
+  // Append to document
+  appendChild(editContactForm, divGroup);
+  appendChild(divGroup, divInputGroup);
+  appendChild(divInputGroup, spanInputGroupLabel);
+  appendChild(divInputGroup, emailInput);
+  appendChild(divInputGroup, trashIcon);
+  appendChild(spanInputGroupLabel, emailIcon);
+
+  // Handle any events
+  addHandler(trashIcon, "click", () => {
+    divInputGroup.remove();
+  });
+});
+
+addHandler(elements.addPhoneButton, "click", () => {
+  log(`\n\tAdded phone field\n`);
+  const editContactForm = elements.editContactForm;
+
+  const divInputGroup = newElement("div");
+  const divGroup = newElement("div");
+  const spanInputGroupLabel = newElement("span");
+  const phoneInput = newElement("input");
+  const trashIcon = newElement("i");
+  const phoneIcon = newElement("i");
+
+  // Add attributes
+  addAttribute(divGroup, "class", "grid-x");
+  addAttribute(divInputGroup, "class", "input-group");
+  addAttribute(spanInputGroupLabel, "class", "input-group-label");
+  addAttribute(phoneInput, "type", "tel");
+  addAttribute(phoneInput, "class", "input-group-field");
+  addAttribute(
+    phoneInput,
+    "name",
+    `email${editContactForm.childNodes.length + 1}`
+  );
+  addAttribute(trashIcon, "class", "fa-solid fa-trash");
+  addAttribute(phoneIcon, "class", "fa-solid fa-phone");
+  addAttribute(trashIcon, "style", "margin-left:5px;margin-top:10px; ");
+
+  // Config elements
+  // spanInputGroupLabel.innerHTML = `<i class="fa-solid fa-envelope"></i>`;
+
+  // Append to document
+  appendChild(editContactForm, divGroup);
+  appendChild(divGroup, divInputGroup);
+  appendChild(divInputGroup, spanInputGroupLabel);
+  appendChild(divInputGroup, phoneInput);
+  appendChild(divInputGroup, trashIcon);
+  appendChild(spanInputGroupLabel, phoneIcon);
+
+  // Handle any events
+  addHandler(trashIcon, "click", () => {
+    divInputGroup.remove();
+  });
+});
